351f2e5b794f971e9358214bb3765935e7bb74d7
anthonyts2 <34192614+anthonyts2@users.noreply.github.com>
2021-04-28 21:30:30 -0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoginValidation();
+        }
+
+        private void LoginValidation()
+        {
+            if(uxName.Text.Length != 0 && uxPassword.Password.Length != 0)
+            {
+                uxSubmit.IsEnabled = true;        
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }    
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LoginValidation();
+        }
+
+        private void uxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            LoginValidation();
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //string username = uxName.Text;
+            //string password = uxPassword.Password;
+            uxName.Clear();
+            uxPassword.Clear();
+            MessageBox.Show("Login Attempted. Username and Password Cleared");
+        }
+
+    }
+}
